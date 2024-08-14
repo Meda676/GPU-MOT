3351c8fe3d4f8ce9b4bd052b4cad6476a196f7f4
biagiop1986 <biagiop1986@gmail.com>
2024-08-14 11:57:40 +0200

    Fixed attempt_time increment bug

--- src/kernels.cpp
+++ src/kernels.cpp
@@ -343,7 +343,7 @@ __global__ void associateAllBIG(float* payloads, int sizePayload, float* measure
             *p_serial_miss += 1.0f;
         }
         if (*p_track_state == 0.0f) 
-                *p_attempt_time++;
+            ++(*p_attempt_time);
     
         if(*p_track_state == 0.0f && *p_life_time >= minDetection)
         {
@@ -478,7 +478,7 @@ __global__ void associateAll(float* payloads, int sizePayload, float* measures,
            *p_serial_miss += 1.0f;
         }
         if (*p_track_state == 0.0f)
-                *p_attempt_time++;
+            ++(*p_attempt_time);
     
         if(*p_track_state == 0.0f && *p_life_time >= minDetection)
         {
